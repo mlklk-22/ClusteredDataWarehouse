de35c6eb671358a04b52624db8ed894f37e3adf0
mlklk-22 <mlkmsbh84@gmail.com>
2023-06-23 22:33:17 +0300

    Create My Own Response For Handling Err/Success Response

---
+++ src/main/java/com/abdelmalek/ClusteredDataWarehouse/DTO/Status.java
@@ -0,0 +1,10 @@
+package com.abdelmalek.ClusteredDataWarehouse.DTO;
+
+import java.time.LocalDate;
+import java.util.Date;
+
+public class Status {
+    public String reasonMsg;
+    public int statusCode;
+    public Date statusDate;
+}
