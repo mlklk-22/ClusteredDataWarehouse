ca6331b076040c6cda4c2e7345b6c6689ab25309
mlklk-22 <mlkmsbh84@gmail.com>
2023-06-23 22:26:28 +0300

    - Change The Response or return value to my own Res "Status" - Add GetAllDeals Api

--- src/main/java/com/abdelmalek/ClusteredDataWarehouse/Controller/DealController.java
+++ src/main/java/com/abdelmalek/ClusteredDataWarehouse/Controller/DealController.java
@@ -1,12 +1,14 @@
 package com.abdelmalek.ClusteredDataWarehouse.Controller;
 
-
+import com.abdelmalek.ClusteredDataWarehouse.DTO.Status;
 import com.abdelmalek.ClusteredDataWarehouse.Model.Deal;
 import com.abdelmalek.ClusteredDataWarehouse.Service.DealService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/Deals")
@@ -17,22 +19,37 @@ public class DealController {
     private DealService _dealService;
 
     @PostMapping("/CreateDeals")
-    public ResponseEntity<Deal> createDeal(@RequestBody Deal deal){
-        Deal createDeal = _dealService.createDeal(deal);
+    public ResponseEntity<Status> createDeal(@RequestBody Deal deal) {
+        Status createDeal = _dealService.createDeal(deal);
+
+        if (createDeal.statusCode == 0)
+            return new ResponseEntity<>(createDeal, HttpStatus.BAD_REQUEST);
+
         return new ResponseEntity<>(createDeal, HttpStatus.CREATED);
     }
 
     @PutMapping("UpdateDeals/{id}")
-    public ResponseEntity<Deal> createDeal(@PathVariable int id, @RequestBody Deal updateddeal){
-        Deal deal = _dealService.updateDeal(id, updateddeal);
+    public ResponseEntity<Status> updateDeal(@PathVariable int id, @RequestBody Deal updateddeal) {
+        Status deal = _dealService.updateDeal(id, updateddeal);
+
+        if (deal.statusCode == 0)
+            return new ResponseEntity<>(deal, HttpStatus.BAD_REQUEST);
+
         return new ResponseEntity<>(deal, HttpStatus.OK);
     }
 
     @DeleteMapping("DeleteDeals/{id}")
-    public ResponseEntity<Void> deleteDeal(@PathVariable int id){
-        _dealService.deleteDeal(id);
-        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    public ResponseEntity<Status> deleteDeal(@PathVariable int id) {
+        Status deleteDeal = _dealService.deleteDeal(id);
+
+        if (deleteDeal.statusCode == 0)
+            return new ResponseEntity<>(deleteDeal, HttpStatus.BAD_REQUEST);
+
+        return new ResponseEntity<>(deleteDeal, HttpStatus.OK);
     }
 
-
+    @GetMapping("/GetAllDeals")
+    public ResponseEntity<List<Deal>> getAllDeals() {
+        return ResponseEntity.ok(_dealService.getAllDeals());
+    }
 }
