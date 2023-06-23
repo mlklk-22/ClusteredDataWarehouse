57aa00577d40fe3fe6cd7754ee4a706197d747b1
mlklk-22 <mlkmsbh84@gmail.com>
2023-06-23 22:27:59 +0300

    - Add GetAllDeals procedure repo - Add IsRequestedTwice procedure repo

--- src/main/java/com/abdelmalek/ClusteredDataWarehouse/Repository/DealRepository.java
+++ src/main/java/com/abdelmalek/ClusteredDataWarehouse/Repository/DealRepository.java
@@ -2,8 +2,16 @@ package com.abdelmalek.ClusteredDataWarehouse.Repository;
 
 import com.abdelmalek.ClusteredDataWarehouse.Model.Deal;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.query.Procedure;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface DealRepository extends JpaRepository <Deal, Integer> {
+    @Procedure("GetAllDeals")
+    List<Deal> GetAllDeals();
+
+    @Procedure("isRequestedTwice")
+    List<Deal> isRequestedTwice(int ID, String from_currency, String to_currency, String date, int deal_amount);
 }
