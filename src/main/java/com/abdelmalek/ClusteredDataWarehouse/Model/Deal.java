5f8dbebfe4704045dc634d7d879439656ed3cfd6
mlklk-22 <mlkmsbh84@gmail.com>
2023-06-23 22:25:10 +0300

    Add Tow Procedure:(GetAllDeals, IsRequestedTwice)

--- src/main/java/com/abdelmalek/ClusteredDataWarehouse/Model/Deal.java
+++ src/main/java/com/abdelmalek/ClusteredDataWarehouse/Model/Deal.java
@@ -1,19 +1,41 @@
 package com.abdelmalek.ClusteredDataWarehouse.Model;
 
+import com.abdelmalek.ClusteredDataWarehouse.DTO.DealValueDTO;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.time.LocalDate;
 import java.util.Date;
 
 @Getter
 @Setter
 @Entity
-@Table(name = "DEAL")
+@Table(name = "Deal")
+@NamedStoredProcedureQueries({
+        @NamedStoredProcedureQuery(
+                name = "GetAllDeals",
+                procedureName = "GetAllDeals",
+                resultClasses = Deal.class
+        ),
+        @NamedStoredProcedureQuery(
+                name = "isRequestedTwice",
+                procedureName = "isRequestedTwice",
+                resultClasses = Deal.class,
+                parameters = {
+                        @StoredProcedureParameter(mode = ParameterMode.IN, name = "ID", type = Integer.class),
+                        @StoredProcedureParameter(mode = ParameterMode.IN, name = "from_currency", type = String.class),
+                        @StoredProcedureParameter(mode = ParameterMode.IN, name = "to_currency", type = String.class),
+                        @StoredProcedureParameter(mode = ParameterMode.IN, name = "date", type = String.class),
+                        @StoredProcedureParameter(mode = ParameterMode.IN, name = "deal_amount", type = Integer.class)
+                }
+        )
+
+})
 public class Deal {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id")
+    @Column(name = "ID")
     private int id;
 
     @Column(name = "from_currency_iso_code")
@@ -23,10 +45,10 @@ public class Deal {
     private String to_currency_iso_code;
 
     @Column(name = "date")
-    private Date date;
+    private String date;
 
     @Column(name = "deal_amount")
-    private String deal_amount;
+    private int deal_amount;
 
 
 
